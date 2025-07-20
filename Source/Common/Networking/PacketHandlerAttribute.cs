5318054ce54173c4501c89efa44c3f0fd02e9385
Michael <5672750+mibac138@users.noreply.github.com>
2025-07-20 04:43:53 +0200

    Extended support for fragmented packets and handling them. Improved download dialog (#584)
    * Minor cleanup
    
    * Extended fragmented packet handling
    
    * Add a detailed download progress display when connecting to a game

--- Source/Common/Networking/PacketHandlerAttribute.cs
+++ Source/Common/Networking/PacketHandlerAttribute.cs
@@ -5,11 +5,19 @@ using JetBrains.Annotations;
 namespace Multiplayer.Common
 {
     [MeansImplicitUse]
+    [AttributeUsage(AttributeTargets.Method)]
     public class PacketHandlerAttribute(Packets packet, bool allowFragmented = false) : Attribute
     {
         public readonly Packets packet = packet;
         public readonly bool allowFragmented = allowFragmented;
     }
 
-    public record PacketHandlerInfo(FastInvokeHandler Method, bool Fragment);
+    [MeansImplicitUse]
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FragmentedPacketHandlerAttribute(Packets packet) : Attribute
+    {
+        public readonly Packets packet = packet;
+    }
+
+    public record PacketHandlerInfo(FastInvokeHandler Method, bool Fragment, FastInvokeHandler? FragmentHandler = null);
 }
