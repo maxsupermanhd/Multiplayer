7b79cc6997fb61d05478d9a4010ec467adc8d54c
notfood <notfood@users.noreply.github.com>
2025-07-20 00:18:49 -0500

    Version 0.11.0 for RW 1.6

--- Source/Common/Version.cs
+++ Source/Common/Version.cs
@@ -2,7 +2,7 @@ namespace Multiplayer.Common
 {
     public static class MpVersion
     {
-        public const string Version = "0.10.6";
+        public const string Version = "0.11.0";
         public const int Protocol = 49;
 
         public const string ApiAssemblyName = "0MultiplayerAPI";
